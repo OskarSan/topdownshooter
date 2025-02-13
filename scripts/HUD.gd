a0bcebe876a349ed7ba308a4b7ad1a4bdc4f34a9
OskarSan <oskar.sankila@gmail.com>
2025-02-13 13:38:56 +0200

    servujuttu toimii

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 @onready var fps_label: Label = $FPSLabel
 @onready var ping_label: Label = $PingLabel
 @onready var debug_label: Label = $DebugLabel
+@onready var address_label: Label = $AddressLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,7 +15,6 @@ func _process(delta: float) -> void:
 	# update FPS
 	var fps = Performance.get_monitor(Performance.TIME_FPS)
 	fps_label.text = "FPS: %d" % fps
-	
 	#update ping
 	if multiplayer.multiplayer_peer and multiplayer.multiplayer_peer is ENetMultiplayerPeer:
 		#var peer_id = multiplayer.get_unique_id()
@@ -23,9 +23,15 @@ func _process(delta: float) -> void:
 		else:
 			# Clients get their ping to the server
 			var packetPeer = multiplayer.get_multiplayer_peer().get_peer(1)
-			#packetPeer = ENetPacketPeer, get_peer(1) gets the server maybe?
+			#packetPeer = ENetPacketPeer, get_peer(1) == the host
+			#
+			#get_statistic(3):
+			#PEER_ROUND_TRIP_TIME = 3
+			#Mean packet round trip time for reliable packets.
+			#
 			ping_label.text = "Ping: %s ms" % packetPeer.get_statistic(3)
+			address_label.text = str("port: %s" % packetPeer.get_remote_port())
 		
-		debug_label.text = str(multiplayer.get_unique_id())
+		debug_label.text = str("id: %d" % multiplayer.get_unique_id())
 	else:
 		ping_label.text = "Ping: N/A"
--- scripts/multiplayer_test.gd
+++ scripts/multiplayer_test.gd
@@ -2,36 +2,50 @@ extends Node2D
 
 
 var peer = ENetMultiplayerPeer.new()
+var port = null
 @export var player_scene: PackedScene
 
 @onready var host_button: Button = $Host
 @onready var join_button: Button = $Join
+@onready var port_input: LineEdit = $PortInput
+
+
 
 func _ready():
+	if DisplayServer.get_name() == "headless":
+		print("Running in headless mode. Creating server automatically...")
+		create_server(1234)
+	else:
+		print("Not running in headless mode. Waiting for user input.")
 	multiplayer.peer_disconnected.connect(_on_peer_disconnected)
+
+func create_server(port: int):
+	self.port = port
+	peer.create_server(port)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(add_player)
 
 func _on_host_pressed() -> void:
 	host_button.hide()
 	join_button.hide()
+	port_input.hide()
 	peer.create_server(3000)
 	multiplayer.multiplayer_peer = peer
 	multiplayer.peer_connected.connect(add_player)
-	
 	add_player(multiplayer.get_unique_id())
-	
+
 func add_player(peer_id):
 	var player = player_scene.instantiate()
 	player.name = str(peer_id)
 	add_child(player)
 
-
 func _on_join_pressed() -> void:
 	host_button.hide()
 	join_button.hide()
-	peer.create_client("localhost", 3000)
+	port_input.hide()
+	peer.create_client("localhost", int(port_input.get_text()))
 	multiplayer.multiplayer_peer = peer
-	
-	
+
 func _on_peer_disconnected(id: int):
 	var player_name = str(id)
 	if has_node(player_name):
